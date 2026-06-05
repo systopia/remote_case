01b818c008bccb87969b3d9b818de1b5f6255b95
Dominic Tubach <tubach@systopia.de>
2026-06-05 16:13:27 +0200

    Set version to 1.0.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/remote_case',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '896e2490f28b751d0579fecee16470ec5670e0cd',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/remote_case' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '896e2490f28b751d0579fecee16470ec5670e0cd',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
